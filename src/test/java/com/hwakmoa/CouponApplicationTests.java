474626b23293e4c655a667e4338c490adbceefd3
jungsunghun <jsh951227@gmail.com>
2022-07-12 23:11:52 +0900

    create repository

---
+++ src/test/java/com/hwakmoa/CouponApplicationTests.java
@@ -0,0 +1,15 @@
+package com.hwakmoa;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertThat(1).isEqualTo(1);
+	}
+
+}
